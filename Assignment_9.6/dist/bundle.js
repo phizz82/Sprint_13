5c2aac0ce112e449662586555a0085b889499b7e
phizz82 <phizz1982@gmail.com>
2023-05-13 15:19:01 +0100

    Additional 9.6 files added

---
+++ Assignment_9.6/dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\n/home/phizz/Sprint_14/Assignment_8.5/babel.config.js:2\\n    \\\"presets\\\": [\\\"@babel/preset-env\\\"]\\n             ^\\n\\nSyntaxError: Unexpected token ':'\\n    at internalCompileFunction (node:internal/vm:73:18)\\n    at wrapSafe (node:internal/modules/cjs/loader:1187:20)\\n    at Module._compile (node:internal/modules/cjs/loader:1231:27)\\n    at Module._extensions..js (node:internal/modules/cjs/loader:1321:10)\\n    at Module.load (node:internal/modules/cjs/loader:1125:32)\\n    at Module._load (node:internal/modules/cjs/loader:965:12)\\n    at Module.require (node:internal/modules/cjs/loader:1149:19)\\n    at require (node:internal/modules/helpers:121:18)\\n    at startHiding - secret - don't use this - v1 (/home/phizz/Sprint_14/Assignment_8.5/node_modules/@babel/core/lib/errors/rewrite-stack-trace.js:54:12)\\n    at loadCjsDefault (/home/phizz/Sprint_14/Assignment_8.5/node_modules/@babel/core/lib/config/files/module-types.js:114:69)\");\n\n//# sourceURL=webpack://assignment_8.5/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
